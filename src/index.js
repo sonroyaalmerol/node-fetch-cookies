2ad37244321f353bd17dce866f0a4a36a667b5cc
sonroyaalmerol <sonroya.almerol@gmail.com>
2020-05-16 23:37:39 +0800

    back

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+const _fetch = require("node-fetch");
 const CookieJar = require("./cookie-jar.js");
 const Cookie = require("./cookie.js");
 const { paramError, CookieParseError } = require("./errors.js");
 
-const _fetch = async (cookieJars, url, options) => {
+const fetch = async (cookieJars, url, options) => {
     let cookies = "";
     const addValidFromJars = jars => {
         // since multiple cookie jars can be passed, filter duplicates by using a set of cookie names
@@ -32,8 +33,7 @@ const _fetch = async (cookieJars, url, options) => {
         options.headers.cookie = cookies.slice(0, -2);
     }
     var opts = { ...options, redirect: 'manual' }
-    var result = null
-    result = await fetch(url, opts);
+    const result = await _fetch(url, opts);
     // I cannot use headers.get() here because it joins the cookies to a string
     cookies = result.headers[Object.getOwnPropertySymbols(result.headers)[0]]["set-cookie"];
     if(cookies && cookieJars) {
@@ -58,10 +58,10 @@ const _fetch = async (cookieJars, url, options) => {
             // we'll hijack it for our internal bookkeeping.
             follow: options.follow !== undefined ? options.follow - 1 : undefined
         })
-        return _fetch(cookieJars, result.headers.get('location'), optsForGet)
+        return fetch(cookieJars, result.headers.get('location'), optsForGet)
     } else {
         return result
     }
 }
 
-module.exports = {fetch: _fetch, CookieJar, Cookie, CookieParseError};
+module.exports = {fetch, CookieJar, Cookie, CookieParseError};
